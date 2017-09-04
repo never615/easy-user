b2684d3a7a12611ab1e23a16d0d05ad96e1fcbde
never615 <583531034@qq.com>
2017-09-04 16:16:56 +0800

    用户表增加操作人id

---
+++ migrations/2017_09_04_150019_add_admin_user_id.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateUsersTable
+ */
+class AddAdminUserId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger("admin_user_id")->nullable();
+            $table->foreign('admin_user_id')->references('id')->on('admin_users')->onDelete('SET NULL');
+        });
+
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn("admin_user_id");
+        });
+
+
+    }
+}
