68b7b51284d775e21e25b032536c23f7791cb147
never615 <583531034@qq.com>
2018-12-26 19:02:35 +0800

    调度任务移动到库内

--- src/Providers/UserServiceProvider.php
+++ src/Providers/UserServiceProvider.php
@@ -6,9 +6,11 @@
 namespace Mallto\User\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
+use Mallto\Tool\Jobs\LogJob;
 use Mallto\User\Domain\UserUsecase;
 use Mallto\User\Domain\UserUsecaseImpl;
 
@@ -93,6 +95,8 @@ class UserServiceProvider extends ServiceProvider
 
         $this->authBoot();
 
+        $this->schedule();
+
 
     }
 
@@ -122,10 +126,10 @@ class UserServiceProvider extends ServiceProvider
 
 
         Passport::tokensCan([
-            'mobile-token'  => 'mobile token可以访问所有需要用户绑定了手机号才能访问的接口',
-            'wechat-token'  => '微信token是通过openId换取的,只能访问部分接口',
-            'parking-token' => '停车需要使用到的token',
-            'account-token' => "账户操作权限:如重新绑定手机",
+            'mobile-token'            => 'mobile token可以访问所有需要用户绑定了手机号才能访问的接口',
+            'wechat-token'            => '微信token是通过openId换取的,只能访问部分接口',
+            'parking-token'           => '停车需要使用到的token',
+            'account-token'           => "账户操作权限:如重新绑定手机",
             'register-complete-token' => "注册信息完善token",
         ]);
     }
@@ -139,6 +143,40 @@ class UserServiceProvider extends ServiceProvider
     public function listens()
     {
         return $this->listen;
+    }
+
+    private function schedule()
+    {
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+
+            //用户数据统计
+            $schedule->command('user:user_statistic')
+                ->onOneServer()
+                ->dailyAt("03:00")
+                ->name("用户统计")
+                ->withoutOverlapping()
+                ->before(function () {
+                    dispatch(new LogJob("logSchedule", ["slug" => "user_statistic", "status" => "start"]));
+                })
+                ->after(function () {
+                    dispatch(new LogJob("logSchedule", ["slug" => "user_statistic", "status" => "finish"]));
+                });
+
+            //拉取微信统计数据
+            $schedule->command('user:wechat_user_statistics')
+                ->onOneServer()
+                ->dailyAt("08:30")
+                ->name("微信统计")
+                ->withoutOverlapping()
+                ->before(function () {
+                    dispatch(new LogJob("logSchedule", ["slug" => "wechat_user_statistics", "status" => "start"]));
+                })
+                ->after(function () {
+                    dispatch(new LogJob("logSchedule", ["slug" => "wechat_user_statistics", "status" => "finish"]));
+                });
+        });
+
     }
 
 }
